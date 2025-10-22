4864d0964a22575e9fa2dcb4bf3b7107ea7ec62b
Jon Swaine <swainejw@limestone.on.ca>
2025-10-22 12:14:54 -0400

    First Upload

---
+++ ScrollingBackround/Rocket.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Person here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rocket extends Actor
+{
+    public Rocket()
+    {
+        turn(270);
+    }
+    public void act() 
+    {
+        if (Greenfoot.isKeyDown("left"))
+        {
+            setLocation(getX() - 4, getY());
+        }
+        if (Greenfoot.isKeyDown("right"))
+        {
+            setLocation(getX() + 4, getY());
+        }
+        if (Greenfoot.isKeyDown("down"))
+        {
+            setLocation(getX(), getY() + 4);
+        }
+        if (Greenfoot.isKeyDown("up"))
+        {
+            setLocation(getX(), getY() - 4);
+        }
+        
+    }    
+}
