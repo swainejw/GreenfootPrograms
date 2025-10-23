ba0229936952a7701c2499d252ceae5796f4ba23
Jon Swaine <swainejw@limestone.on.ca>
2025-10-23 12:52:46 -0400

    Shooting Fish

--- Grade10FirstProgram2025/Leaf.java
+++ Grade10FirstProgram2025/Leaf.java
@@ -2,13 +2,27 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Leaf extends Actor
 {
+    public Leaf()
+    {
+        getImage().scale(10,10);
+    }
+    
     public void act()
     {
         setLocation(getX() + 5, getY());
         
+        Fish f = (Fish) getOneIntersectingObject(Fish.class);
+        if (f != null)
+        {
+            getWorld().removeObject(f);
+            getWorld().removeObject(this);
+            return;
+        }
+        
         if (getX() > 590)
         {
             getWorld().removeObject(this);
+            return;
         }
     }
 }
