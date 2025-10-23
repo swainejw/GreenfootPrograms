26eef92137c69d3f2db634be88a158a629287dc8
Jon Swaine <swainejw@limestone.on.ca>
2025-10-23 13:09:40 -0400

    Fish Bounce

--- Grade10FirstProgram2025/Fish.java
+++ Grade10FirstProgram2025/Fish.java
@@ -1,19 +1,16 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Fish here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Fish extends Actor
 {
-    /**
-     * Act - do whatever the Fish wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    int yMove = 5;
+    
     public void act()
     {
-        // Add your action code here.
+        setLocation(getX(), getY() - yMove);
+        
+        if (getY() < 10 || getY() > 390)
+        {
+            yMove *= -1;
+        }
     }
 }
