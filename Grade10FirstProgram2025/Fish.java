a3abfca1bb770af1496b2e9cce3f652f192efe8b
Jon Swaine <swainejw@limestone.on.ca>
2025-10-27 13:07:07 -0400

    Oct27p2

--- Grade10FirstProgram2025/Fish.java
+++ Grade10FirstProgram2025/Fish.java
@@ -3,6 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Fish extends Actor
 {
     int yMove = 5;
+    SimpleTimer tFish = new SimpleTimer();
+    int rn = Greenfoot.getRandomNumber(3000);
     
     public void act()
     {
@@ -11,6 +13,13 @@ public class Fish extends Actor
         if (getY() < 10 || getY() > 390)
         {
             yMove *= -1;
+        }
+        
+        if (tFish.millisElapsed() > rn)
+        {
+            getWorld().addObject(new Seaweed(), getX(), getY());
+            tFish.mark();
+            rn = Greenfoot.getRandomNumber(3000);
         }
     }
 }
---
+++ Grade10FirstProgram2025/Seaweed.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Seaweed here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Seaweed extends Actor
+{
+    /**
+     * Act - do whatever the Seaweed wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX() - 5, getY());
+        
+        if (getX() < 10)
+        {
+            getWorld().removeObject(this);
+            return;
+        }
+        
+        Frog f = (Frog) getOneIntersectingObject(Frog.class);
+        if (f != null)
+        {
+            getWorld().removeObject(f);
+            getWorld().removeObject(this);
+            return;
+        }
+    }
+}
