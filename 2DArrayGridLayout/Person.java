955a709fd4adf8b65b60c752b66845025daf0ee0
Jon Swaine <swainejw@limestone.on.ca>
2025-11-07 14:18:39 -0500

    Nov72025

--- 2DArrayGridLayout/Person.java
+++ 2DArrayGridLayout/Person.java
@@ -8,10 +8,13 @@ public class Person extends Actor
     
     public void act()
     {
+        // move down always by dy - that value controls "gravity"
         setLocation(getX(), getY() - dy);
         
         if (Greenfoot.isKeyDown("right"))
         {
+            // only have the player move if not yet at what we define as the "edge"
+            // at that point, it's the background that moves!
             if (getX() < getWorld().getWidth() - edge)
             {
                 setLocation(getX() + speed, getY());
@@ -22,9 +25,12 @@ public class Person extends Actor
                     setLocation(getX() - speed, getY());
                 }
             }
+            // if the last horizontal object (the 99th in the array)has an x value above
+            // 975, we continue looking to see if we need to move the background
             else if (MyWorld.bg[99][0].getX() > 975)
             {
                 Brick bHR = (Brick) getOneObjectAtOffset(getImage().getWidth()/2 + speed, 0, Brick.class);
+                // if we have NOT hit a brick, move the bg objects
                 if (bHR == null) 
                 {
                     MyWorld.moveBGHoriz(-speed);
@@ -32,6 +38,7 @@ public class Person extends Actor
             }
         }
         
+        // see comments for right movement
         if (Greenfoot.isKeyDown("left"))
         {
             if (getX() > edge)
@@ -53,20 +60,24 @@ public class Person extends Actor
             }
         }
         
+        // if on the ground and space bar, we "jump" by changing dy
         if (Greenfoot.isKeyDown("space") && onGround())
         {
             dy = 23;
             setLocation(getX(), getY() - 1);
         }
         
+        // if dy is below , we're moving down
         if (dy <= 0)
         {
+            // lift the player out of the grass or brick one pixel at a time
             while (isTouching(Grass.class) || isTouching(Brick.class))
             {
                 setLocation(getX(), getY() - 1);
             }
         }
         
+        // if not on the ground, fall.  otherwise, don't.
         if (!onGround())
         {
             dy--;
@@ -78,6 +89,8 @@ public class Person extends Actor
         
     }
     
+    // definition of onGround is 1 pixel above the brick or grass
+    // returns true (on ground) or false (not on ground)
     public boolean onGround()
     {
         for (int x = -getImage().getWidth()/2; x < getImage().getWidth()/2; x++)
