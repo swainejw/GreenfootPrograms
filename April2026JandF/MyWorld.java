484f2632455383f4018666981fb6b1d8c38263e2
Jon Swaine <swainejw@limestone.on.ca>
2026-04-30 12:18:44 -0400

    Jumping and Falling v3

--- April2026JandF/MyWorld.java
+++ April2026JandF/MyWorld.java
@@ -3,13 +3,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     public static Steve stv = new Steve();
+    SimpleTimer t = new SimpleTimer();
     
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         addObject(stv, 40, getHeight() - 50);
-        
         // Make a row of bricks (like a platform) at y-coord 30 above the bottom)
         // offset = 25, spacing =  50
         for (int x = 0; x < 15; x++)
@@ -19,12 +19,21 @@ public class MyWorld extends World
         
         for (int x = 0; x < 8; x++)
         {
-            addObject(new Brick(), 25 + x*50, getHeight() - 150);
+            addObject(new Brick(), 25 + x*50, getHeight() - 120);
         }
         
         for (int x = 4; x < 15; x++)
         {
             addObject(new Brick(), 25 + x*50, getHeight() - 250);
+        }
+    }
+    
+    public void act()
+    {
+        if (t.millisElapsed() > 4000)
+        {
+            addObject(new Barrel(), getWidth() - 50, 40);
+            t.mark();
         }
     }
 }
